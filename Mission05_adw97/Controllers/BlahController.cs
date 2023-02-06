c6c28a27150e5c78cbe2febdbf6bc6dd2670a2eb
wirickad <92128478+wirickad@users.noreply.github.com>
2023-02-06 11:00:33 -0700

    Add project files.

---
+++ Mission05_adw97/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission05_adw97.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//This controller is where we set the routes
+namespace Mission05_adw97.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form (GradeFormModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission05_adw97/Models/GradeFormModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Here, we define the model for the GradeForm. This way we know what inputs to expect, and how to handle them on the back-end. 
+namespace Mission05_adw97.Models
+{
+    public class GradeFormModel
+    {
+        [Required]
+        [Range(0,100,ErrorMessage = "Assignments value must be between 0 and 100")]
+        public int Assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Projects value must be between 0 and 100")]
+        public int Projects { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Quizzes value must be between 0 and 100")]
+        public int Quizzes { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Midterm value must be between 0 and 100")]
+        public int Midterm { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Final value must be between 0 and 100")]
+        public int Final { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Intex value must be between 0 and 100")]
+        public int Intex { get; set; }
+
+        public int FinalGrade;
+    }
+}
